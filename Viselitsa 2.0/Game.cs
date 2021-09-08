de442994a5305ac2958f8899ed4aaf5b48d24a68
Nek0_0 <16bitstreams@gmail.com>
2021-09-08 22:49:31 +0300

    Logical Part (not finished)

---
+++ Viselitsa 2.0/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viselitsa_2._0
+{
+    public class Game
+    {
+        public static int Count { get; set; }
+        public static void Guess()
+        {
+            var encoded = Word.EncodeWord();
+            Count = encoded.Letters.Length;
+            Console.WriteLine("Счёт: " + Count);
+            do
+            {
+                char let = Console.ReadLine()[0];
+                for (int i = 0; i < encoded.Letters.Length; i++)
+                {
+                    if (let == encoded.Letters[i])
+                    {
+                        encoded.Lines[i] = let;
+                        Console.Clear();
+                        Print(encoded.Lines);
+                        Console.WriteLine("Cчёт: "+ Count);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Print(encoded.Lines);
+                        Count = Count - 1;
+                        Console.WriteLine("Cчёт: " + Count);
+                    }
+                }
+            } while (1 > 0);
+        }
+        static void Print(char[] array)
+        {
+            foreach(var let in array)
+            {
+                Console.Write(let + " ");
+            }
+        }
+            
+    }
+}
---
+++ Viselitsa 2.0/Word.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Viselitsa_2._0
+{
+    public class Word
+    {
+        public char[] Letters { get; set; }
+        public char[] Lines { get; set; }
+        public bool IsGuessed { get; set; }
+        public Word(char[] letters,char[] lines, bool isGuessed)
+        {
+            Letters = letters;
+            Lines = lines;
+            IsGuessed = isGuessed;
+        }
+
+        public static string GetWord()
+        {
+            string[] words = File.ReadAllLines("words.txt");
+            string word;
+            Random rnd = new Random();
+            return word = words[rnd.Next(0, words.Length)];
+        }
+
+        public static Word EncodeWord()
+        {
+            var current = GetWord().ToCharArray();
+            Word word = new Word(current, current, false);
+            word.Lines = word.Lines.Select(x => '_').ToArray();
+            for (int i = 0; i < word.Letters.Length; i++)
+            {
+                
+                Console.Write("_ ");
+            }
+            return word;
+        }
+    }
+}
